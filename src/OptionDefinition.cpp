0d2679665b871ee77623f36523be74931acfdf3f
gav@dhcp151.well.ox.ac.uk <gav@dhcp151.well.ox.ac.uk@noemail.net>
2009-06-29 15:28:26 +0000

    Whoops: added these files which should be from the OPtionProcessor refactor a few versions back.
    FossilOrigin-Name: 7d623519a1ae58871f1de9446817f2886abe863320684c162d46c3bc34d9aaac

---
+++ src/OptionDefinition.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <sstream>
+#include <cassert>
+#include "OptionDefinition.hpp"
+#include "OptionProcessor.hpp"
+
+
+OptionDefinition::OptionDefinition()
+	: m_description(""),
+		m_is_required( false ),
+		m_number_of_values_per_use( 0 ),
+		m_lower_number_of_permitted_values( 0 ),
+		m_upper_number_of_permitted_values( 0 ),
+		m_has_default_value( false ),
+		m_default_value( "" )
+{}
+
+OptionDefinition::OptionDefinition( OptionDefinition const& other )
+	: m_description( other.m_description ),
+		m_is_required( other.m_is_required ),
+		m_number_of_values_per_use( other.m_number_of_values_per_use ),
+		m_lower_number_of_permitted_values( other.m_lower_number_of_permitted_values ),
+		m_upper_number_of_permitted_values( other.m_upper_number_of_permitted_values ),
+		m_has_default_value( other.m_has_default_value ),
+		m_default_value( other.m_default_value )
+{}
+
+std::vector< std::string > OptionDefinition::preprocess_option_values( std::string const& option_name, std::vector< std::string > const& option_values ) const {
+	std::vector< value_preprocessor_t >::const_iterator
+		i = m_value_preprocessors.begin(),
+		end_i = m_value_preprocessors.end() ;
+	std::vector< std::string > result = option_values ;
+	for( ; i != end_i; ++i ) {
+		result = (*i)( option_name, option_values ) ;
+	}
+	return result ;
+}
+
+void OptionDefinition::check_option_values( std::string const& option_name, std::vector< std::string > const& option_values ) const {
+
+	// Check if number or values is too few...
+	if( option_values.size() < m_lower_number_of_permitted_values ) {
+		std::ostringstream ostr ;
+		ostr << "Option \"" << option_name << "\" requires at least " << m_lower_number_of_permitted_values << " value" ;
+		if( m_lower_number_of_permitted_values > 1 ) ostr << "s" ;
+		
+		throw OptionValueInvalidException( option_name, option_values, ostr.str() ) ;
+	}
+
+	// ...or too many.
+	if( option_values.size() > m_upper_number_of_permitted_values ) {
+		std::ostringstream ostr ;
+		ostr << "Option \"" << option_name << "\" takes at most " << m_upper_number_of_permitted_values << " value" ;
+		if( m_upper_number_of_permitted_values > 1 ) ostr << "s" ;
+
+		throw OptionValueInvalidException( option_name, option_values, ostr.str() ) ;
+	}
+	
+
+	std::vector< value_checker_t >::const_iterator
+		i = m_value_checkers.begin(),
+		end_i = m_value_checkers.end() ;
+	for( ; i != end_i; ++i ) {
+		(*i)( option_name, option_values ) ;
+	}
+}
---
+++ src/OptionProcessor.cpp
@@ -0,0 +1,264 @@
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include "GenRow.hpp"
+#include "GToolException.hpp"
+#include "OptionDefinition.hpp"
+#include "OptionProcessor.hpp"
+
+OptionProcessingException::OptionProcessingException( std::string option, std::vector< std::string > values, std::string msg )
+: 	m_option( option ),
+	m_values( values ),
+	m_msg( msg )
+{}
+
+OptionProcessingException::OptionProcessingException( std::string option, std::string msg )
+: 	m_option( option ),
+	m_msg( msg )
+{}
+
+OptionProcessingException::~OptionProcessingException() throw()
+{} ;
+
+OptionParseException::OptionParseException( std::string option, std::vector< std::string > values, std::string msg )
+: OptionProcessingException( option, values, msg )
+{}
+
+OptionValueInvalidException::OptionValueInvalidException( std::string option, std::vector< std::string > values, std::string msg )
+: OptionProcessingException( option, values, msg )
+{}
+
+
+char const* OptionProcessingException::OptionProcessingException::what() const throw() {
+	try {
+		std::ostringstream ostr ;
+		if( m_option.size() > 0 ) {
+			ostr << "For option \"" << m_option << "\": " ;
+		}
+		ostr
+			<< m_msg
+			<< " (values supplied:" ;
+		for( std::size_t i = 0; i < m_values.size() ; ++i ) {
+			if( i > 0 )
+				ostr << "," ;
+			ostr << m_values[i] ;
+		}
+		ostr << ")" ;
+		set_message( ostr.str() ) ;
+		return GToolException::what() ;
+	}
+	catch (... ) {
+		return "OptionProcessingException::what(): exception formatting string." ;
+	}
+}
+
+
+std::ostream& operator<<( std::ostream& aStream, OptionProcessor::OptionDefinitions const& option_definitions ) {
+    // Find longest option and longest option description
+    OptionProcessor::OptionDefinitions::const_iterator
+        defn_i = option_definitions.begin(),
+        defn_end = option_definitions.end() ;
+
+    std::size_t max_option_length = 0, max_description_length = 0 ;
+     
+    for( ; defn_i != defn_end; ++defn_i ) {
+        max_option_length = std::max( max_option_length, defn_i->first.size()) ;
+        max_description_length = std::max( max_description_length, defn_i->second.description().size() ) ;
+    }
+
+	// print options
+    defn_i = option_definitions.begin() ;
+    for( ; defn_i != defn_end; ++defn_i ) {
+        aStream << std::setw( max_option_length+1 )
+                << std::right
+                << defn_i->first
+                << " " ;
+		
+		std::ostringstream flags ;
+        if( defn_i->second.is_required() ) {
+            flags << "required" ;
+		}
+        if( defn_i->second.takes_values() ) {
+			if( flags.str().size() > 0 ) {
+				flags << "," ;
+			}
+			flags << "takes values";
+		}
+		if( defn_i->second.has_default_value()) {
+			if( flags.str().size() > 0 ) {
+				flags << "," ;
+			}
+			flags << "default: " << defn_i->second.default_value() ;
+		}
+
+        aStream << ":\t"
+                << std::setw( max_description_length+1 )
+                << std::left
+                << defn_i->second.description()
+                << "\t"
+				<< "(" + flags.str() + ")"
+				<< ".\n" ;
+    }
+
+    return aStream ;
+}
+
+
+OptionProcessor::OptionProcessor() {}
+OptionProcessor::OptionProcessor( OptionProcessor const& other )
+	: m_option_definitions( other.m_option_definitions ),
+		m_option_values( other.m_option_values ),
+		m_unknown_options( other.m_unknown_options )
+{}
+
+OptionProcessor::~OptionProcessor() {} ;
+		
+OptionProcessor::OptionDefinitions const& OptionProcessor::option_definitions() const { return m_option_definitions ; }
+
+OptionDefinition& OptionProcessor::operator[]( std::string const& arg ) {
+	return m_option_definitions[ arg ] ;
+}
+
+OptionDefinition const& OptionProcessor::operator[] ( std::string const& arg ) const {
+	OptionDefinitions::const_iterator defn_i = m_option_definitions.find( arg ) ;
+	if( defn_i == m_option_definitions.end() )
+		throw OptionValueInvalidException( arg, std::vector< std::string >(), "Option \"" + arg + "\" is not defined." ) ;
+	return defn_i->second ;
+}
+
+// Parse the options from argv, performing all needed checks.
+void OptionProcessor::process( int argc, char** argv ) {
+	parse_options( argc, argv ) ;
+	check_required_options_are_supplied() ;
+	preprocess_option_values() ;
+	check_option_values() ;
+}
+
+// Parse the options.  Store option values.  Ignore, but store unknown args for later reference
+void OptionProcessor::parse_options( int argc, char** argv ) {
+	int i = 0;
+	while( ++i < argc ) {
+		std::string arg = argv[i] ;
+		std::map< std::string, OptionDefinition >::const_iterator arg_i = m_option_definitions.find( arg ) ;
+		if( arg_i != m_option_definitions.end() ) {
+			for( unsigned int value_i = 0; value_i < arg_i->second.number_of_values_per_use(); ++value_i ) {
+				if( ++i == argc ) {
+					std::ostringstream ostr ;
+					ostr << "Option\"" << arg << "\" takes " << arg_i->second.number_of_values_per_use() << " values per use." ;
+					throw OptionParseException( arg, m_option_values[ arg ], ostr.str() ) ;
+				}
+				else {
+					m_option_values[ arg ].push_back( argv[i] ) ;
+				}
+			}
+		}
+		else
+		{
+			m_unknown_options.insert( argv[i] ) ;
+		}
+	}
+	
+	if( !m_unknown_options.empty()) {
+		std::string unknown_args ;
+		std::set< std::string >::const_iterator i = m_unknown_options.begin() ;
+		for( ; i != m_unknown_options.end(); ++i ) {
+			if( i != m_unknown_options.begin() )
+				unknown_args += ", " ;
+			unknown_args += *i ;
+		}
+		throw OptionParseException( "", std::vector< std::string >(), "The following options were not recognised: " + unknown_args + "." ) ;
+	}
+}
+
+void OptionProcessor::check_required_options_are_supplied() {
+	std::map< std::string, OptionDefinition >::const_iterator
+		defn_i = m_option_definitions.begin(), 
+		defn_end = m_option_definitions.end() ;
+
+	for( ; defn_i != defn_end; ++defn_i ) {
+		if( defn_i->second.is_required() && !check_if_option_was_supplied( defn_i->first ) && !defn_i->second.has_default_value()) {
+			throw OptionProcessingException( defn_i->first, std::vector< std::string >(), "Option \"" + defn_i->first + "\" must be supplied." ) ;
+		}
+	}
+}
+
+void OptionProcessor::preprocess_option_values() {
+	OptionValues::iterator
+		arg_i = m_option_values.begin(),
+		arg_end = m_option_values.end() ;
+
+	for( ; arg_i != arg_end; ++arg_i ) {
+		OptionDefinitions::const_iterator defn_i
+			= m_option_definitions.find( arg_i->first ) ;
+	
+		arg_i->second = defn_i->second.preprocess_option_values( arg_i->first, arg_i->second ) ;
+	}
+}
+
+void OptionProcessor::check_option_values() {
+	OptionValues::const_iterator
+		arg_i = m_option_values.begin(),
+		arg_end = m_option_values.end() ;
+
+	for( ; arg_i != arg_end; ++arg_i ) {
+		OptionDefinitions::const_iterator defn_i
+			= m_option_definitions.find( arg_i->first ) ;
+
+		// Run user-supplied checker if supplied
+	   defn_i->second.check_option_values( arg_i->first, arg_i->second ) ; 
+	}
+}
+
+// check if the given option (which must be valid) was supplied.
+bool OptionProcessor::check_if_option_was_supplied( std::string const& arg ) const {
+	return ( m_option_values.find( arg ) != m_option_values.end() ) ;
+}
+
+std::string OptionProcessor::get_default_value( std::string const& arg ) const {
+	std::map< std::string, OptionDefinition >::const_iterator defn_i
+		= m_option_definitions.find( arg ) ;
+	assert( defn_i != m_option_definitions.end() ) ;
+	assert( defn_i->second.has_default_value() ) ;
+	return defn_i->second.default_value() ;
+}
+
+std::ostream& operator<<( std::ostream& aStream, OptionProcessor const& options ) {
+	return aStream << "Options:\n" << options.option_definitions() ;
+}
+
+// get the values of the given option as a whitespace-separated string
+// Note: from this function, it may not possible to determine if an option value
+// contained whitespace or if several values were supplied.  Use the std::vector form
+// below to avoid ambiguity.
+template<>
+std::string OptionProcessor::get_value( std::string const& arg ) const {
+	OptionValues::const_iterator arg_i ;
+	arg_i = m_option_values.find( arg ) ;
+	
+	if( arg_i == m_option_values.end() ) {
+		return get_default_value( arg ) ;
+	}
+
+	assert( arg_i->second.size() == 1 ) ;
+	return arg_i->second[0] ;
+}
+
+// get the value(s) of the given option as a vector of strings.
+template<>
+std::vector< std::string > OptionProcessor::get_values< std::string >( std::string const& arg ) const {
+	OptionValues::const_iterator arg_i ;
+	arg_i = m_option_values.find( arg ) ;
+	
+	if( arg_i == m_option_values.end() ) {
+		return std::vector< std::string > ( 1, get_default_value( arg )) ;
+	}
+
+	return arg_i->second ;
+}
+
+
