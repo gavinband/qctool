47809309ccc86a4d4b2714ba6c23d6849d82d055
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2010-06-07 14:33:30 +0000

    genfile::CohortIndividualSource: check uniqueness of id_1 / id_2 pairs.
    FossilOrigin-Name: 6de7f2d9f9f4243d2d6e2ae8dd92b19f22d46d7b45e01d91b06185285c6d107e

--- genfile/src/CategoricalCohortIndividualSource.cpp
+++ genfile/src/CategoricalCohortIndividualSource.cpp
@@ -67,28 +67,16 @@ namespace genfile {
 	}
 	
 	void CategoricalCohortIndividualSource::check_sample_ids() const {
-		// each of id_1 and id_2 fields should be unique.
+		// id_1 and id_2 fields, put together, should be unique.
 		{
 			std::set< std::string > sample_ids ;
 			for( std::size_t i = 0; i < get_number_of_individuals(); ++i ) {
-				std::string id = get_entry( i, "id_1" ).as< std::string >() ;
+				std::string id = get_entry( i, "id_1" ).as< std::string >() + " " + get_entry( i, "id_2" ).as< std::string >();
 				if( sample_ids.find( id ) != sample_ids.end() ) {
-					throw DuplicateKeyError( get_filename(), i + 2, find_column_name( "id_1" )) ;
-				}
-				sample_ids.insert( id ) ;
-			}
-		}
-
-		{
-			std::set< std::string > sample_ids ;
-			for( std::size_t i = 0; i < get_number_of_individuals(); ++i ) {
-				std::string id = get_entry( i, "id_2" ).as< std::string >() ;
-				if( sample_ids.find( id ) != sample_ids.end() ) {
-					throw DuplicateKeyError( get_filename(), i + 2, find_column_name( "id_2" ) ) ;
+					throw DuplicateKeyError( get_filename(), i + 2, find_column_name( "id_1" ) ) ;
 				}
 				sample_ids.insert( id ) ;
 			}
 		}
 	}
-	
 }
