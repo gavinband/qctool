12342e6be30cb7e9c34aa0b76187d892aca0a847
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-01-28 13:05:53 +0000

    Updated info.R, this seems to work fine.
    FossilOrigin-Name: 3bbcc6be619aebb0e96a6a8a780ae06da9743efbebfa15310d34253484f1aed6

--- analysis/info.R
+++ analysis/info.R
@@ -14,6 +14,9 @@ info.original <- function( genotypes ) {
     f = genotypes[, 2] + 4 * genotypes[, 3]
     e = genotypes[, 2] + 2 * genotypes[, 3]
     theta = sum(e) / (2*I)
+    if( theta == 0 || theta == 1 ) {
+        return( 1 )
+    }
     ratio = sum( f-e^2 ) / ( 2*N*theta*(1-theta) )
     return( 1 - ratio )
 }
@@ -43,6 +46,9 @@ info.variant1 <- function( genotypes ) {
     adj2 = ( 1.0 - rowSums( genotypes ) )^2
 
     theta = sum(e) / (2*I)
+    if( theta == 0 || theta == 1 ) {
+        return( 1 )
+    }
     ratio = sum( f-e^2 ) / ( 2*N*theta*(1-theta) )
 
     numerator = sum( f - e^2 ) + (( N - I ) * ( 2 * theta * ( 1 + theta ))) - 4 * theta * sum( adj1 ) - 4 * theta^2 * sum( adj2 )
@@ -80,6 +86,9 @@ info.variant2 <- function( g ) {
     N = nrow( g )
     e = g[,2] + 2*g[,3]
     theta.hat = sum(e) / ( 2 * I )
+    if( theta.hat == 0 || theta.hat == 1 ) {
+        return( 1 )
+    }
 
     vF0 = sum(g[,1]*(1-g[,1]))
     vF1 = sum(g[,2]*(1-g[,2]))
@@ -127,9 +136,116 @@ info.variant2.terms <- function( g ) {
     return( list( info = ( 1 - ( numerator / denominator )), variance.terms = numerator1, covariance.terms = numerator2, denominator = denominator ))
 }
 
-X = read.table( "PS_01_illumina.top.snp-stats" )
+load.snp.stats <- function( filename, info_filename = NULL ) {
+    X = read.table( filename )
+    X$I = rowSums( X[, c( "AA", "AB", "BB" )])
 
-genotypes = read.table( "PS_01_illumina.top.gen", hea=F, as.is = T )
+    if( !is.null( info_filename )) {
+        info = read.table( info_filename, as.is = T, header = F )
+        colnames( info )[c(1:3,7)] = c( "SNPID", "rsid", "position", "type" )
+        stopifnot( X$rsid == info$rsid )
+        X$type = info$type
+        X$type = factor( X$type, levels = c( 1, 2 ))
+        levels( X$type ) = c( "imputed", "genotyped" )
+    } else {
+        X$type = factor( "genotyped", levels = c( "imputed", "genotyped" ))
+    }
+
+    X = melt( X, measure.vars = c("information", "jonathans_information" ))
+    X$variable = as.character( X$variable )
+    X$variable[ which( X$variable == "information" )] = "info.variant1"
+    X$variable[ which( X$variable == "jonathans_information" )] = "info.variant2"
+    return( X )
+}
+
+library( ggplot2 )
+
+genotyped = load.snp.stats( "PS_01_illumina.snp-stats" )
+imputed = load.snp.stats( "PS.illumina.22.80.500.imputed.gz.snp-stats", "PS.illumina.22.80.500.info" )
+
+make.plot <- function( data ) {
+    #X11( type = "Xlib" )
+    p = ggplot( data = data )
+    p = p + layer( mapping = aes( x = old_information, y = value, colour = MAF, shape = type, solid = type ), geom = "point" )
+    p = p + scale_shape_manual( value =  c( 20, 3 ) )
+    #p = p + scale_colour_manual( values = c( "red", "lightblue" ) )
+    #p = p + stat_abline( xintercept = 0, slope = 1, colour = "red" )
+    p = p + facet_wrap( ~ variable )
+    p = p + xlab( "Original information measure" )
+    p = p + ylab( "New information measure" )
+    p = p + scale_colour_continuous( high = "black", low = "red" )
+    return( p )
+}
+p = make.plot( imputed )
+p = p + opts( title = "(PS chr22 imputed / genotyped data) info variant versus original" )
+#print(p)
+print(p + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ) )
+ggsave( p, file = "PS.illumina.22.80.500.imputed_info_comparison.png", dpi = 72, width = 16, height = 8 )
+ggsave( p + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ), file = "PS.illumina.22.80.500.imputed_info_comparison_zoom.png", dpi = 72, width = 16, height = 8 )
+
+q = make.plot( genotyped )
+q = q + opts( title = "(PS chr1 genotyped data) info variant versus original" )
+print( q + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ) )
+ggsave( q + scale_x_continuous( limits = c( 0.0, 1.0 ) ) + scale_y_continuous( limits = c( 0.0, 1.0 ) ), file = "PS_01_info_comparison.png", dpi = 72, width = 16, height = 8 )
+ggsave( q + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ), file = "PS_01_info_comparison_zoom.png", dpi = 72, width = 16, height = 8 )
+
+genotyped$missing_category = cut( genotyped$I, breaks = c( 0, 1000, 2000, 2500, 2600, 2610, 2620, 2622 ), labels = c( "I >= 0", "I >= 1000", "I >= 2000", "I >= 2500", "I >= 2600", "I >= 2610", "I >= 2620" ))
+genotyped$MAF_category = cut( genotyped$MAF, breaks = c( -1, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5 ), labels = c( "MAF < 0.01", "MAF < 0.1", "MAF < 0.2", "MAF < 0.3", "MAF < 0.4", "MAF < 0.5" ))
+
+p = ggplot( data = genotyped[ which( !is.na( genotyped$MAF )), ] )
+p = p + layer( mapping = aes( x = old_information, y = value, colour = -log10( missing ), shape = MAF_category ), geom = "point" )
+#p = p + scale_colour_manual( values = c( "red", "lightblue" ) )
+#p = p + stat_abline( xintercept = 0, slope = 1, colour = "red" )
+p = p + facet_wrap( ~ variable )
+p = p + xlab( "Original information measure" )
+p = p + ylab( "New information measure" )
+p = p + scale_colour_continuous( high = "black", low = "red" )
+print(p + scale_x_continuous( limits = c( 0.0, 1.0 ) ) + scale_y_continuous( limits = c( 0.0, 1.0 ) ))
+ggsave( p + scale_x_continuous( limits = c( 0.0, 1.0 ) ) + scale_y_continuous( limits = c( 0.0, 1.0 ) ), file = "PS_01_info_comparison_coloured_by_missingness.png", dpi = 72, width = 16, height = 16 )
+ggsave( p + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ), file = "PS_01_info_comparison_coloured_by_missingness_zoom.png", dpi = 72, width = 16, height = 16 )
+
+p = ggplot( data = genotyped[ which( !is.na( genotyped$MAF )), ] )
+p = p + layer( mapping = aes( x = old_information, y = value, colour = -log10( missing ) ), geom = "point" )
+#p = p + scale_shape_manual( value =  c( 20, 3 ) )
+#p = p + scale_colour_manual( values = c( "red", "lightblue" ) )
+#p = p + stat_abline( xintercept = 0, slope = 1, colour = "red" )
+p = p + facet_grid( MAF_category ~ variable )
+p = p + xlab( "Original information measure" )
+p = p + ylab( "New information measure" )
+p = p + scale_colour_continuous( high = "black", low = "red" )
+p = p + opts( title = "(PS chr1 genotyped data) Info variants vs original,facetted by MAF")
+print(p + scale_x_continuous( limits = c( 0.0, 1.0 ) ) + scale_y_continuous( limits = c( 0.0, 1.0 ) ) )
+ggsave( p + scale_x_continuous( limits = c( 0.0, 1.0 ) ) + scale_y_continuous( limits = c( 0.0, 1.0 ) ), file = "PS_01_info_by_MAF.png", dpi = 72, width = 16, height = 24 )
+ggsave( p + scale_x_continuous( limits = c( 0.9, 1.0 ) ) + scale_y_continuous( limits = c( 0.9, 1.0 ) ), file = "PS_01_info_by_MAF_zoom.png", dpi = 72, width = 16, height = 24 )
+
+snp.stats = read.table( "PS_01_illumina.snp-stats" )
+snp.stats$MAF_category = cut( snp.stats$MAF, breaks = c( -1, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5 ), labels = c( "MAF < 0.01", "MAF < 0.1", "MAF < 0.2", "MAF < 0.3", "MAF < 0.4", "MAF < 0.5" ))
+snp.stats$I = rowSums( snp.stats[, c( "AA", "AB", "BB" )] )
+snp.stats$I_category = cut( snp.stats$I, breaks = c( 0, 1000, 2000, 2500, 2600, 2610, 2620, 2625 ), labels = c( "I < 1000", "I < 2000", "I < 2500", "I < 2600", "I < 2610", "I < 2620", "I <= 2622" ))
+
+p = ggplot( data = snp.stats[ which( !is.na( snp.stats$MAF )), ] )
+p = p + layer( mapping = aes( x = jonathans_information, y = information, colour = -log10( missing ), shape = MAF_category ), geom = "point" )
+#p = p + scale_shape_manual( value =  c( 20, 3 ) )
+#p = p + scale_colour_manual( values = c( "red", "lightblue" ) )
+#p = p + stat_abline( xintercept = 0, slope = 1, colour = "red" )
+p = p + xlab( "info variant2" )
+p = p + ylab( "info variant1" )
+#p = p + facet_wrap( ~MAF_category )
+
+p = p + scale_colour_continuous( high = "black", low = "red" )
+p = p + opts( title = "(PS chr01 genotyped data).  Info variant 1 vs variant 2.")
+print(p)
+ggsave( p, file = "PS_01_info_variant1_vs_variant2.png", dpi = 72, width = 8, height = 8 )
+
+which( genotyped$rsid %in% imputed$rsid )
+
+##########################
+
+
+genotypes = read.table( gzfile( "PS_01_illumina.top.gen.gz" ), hea=F, as.is = T )
+colnames( genotypes )[1:5] = c( "SNPID", "rsid", "pos", "allele1", "allele2" )
+
+genotypes = read.table( "PS_master_01_illumina.top.gen", hea=F, as.is = T )
 colnames( genotypes )[1:5] = c( "SNPID", "rsid", "pos", "allele1", "allele2" )
 
 D = data.frame(
@@ -142,11 +258,20 @@ D = data.frame(
     original = NA,
     variant1 = NA,
     variant2 = NA,
-    strange = NA
+    strange = NA,
+    original.flipped = NA,
+    variant1.flipped = NA,
+    variant2.flipped = NA,
+    strange.flipped = NA
 )
 
-for( i in 1:10) {
+for( i in 1:100) {
+    if( i %% 10 == 0 ) {
+        cat( i, 'of', nrow(D), '...' )
+    }
     G = matrix( data = as.numeric( genotypes[i, 6:ncol( genotypes) ]), ncol = 3, byrow = T )
+    G.flipped = G[, c(3,2,1)]
+
     D$AA[i] = sum( G[, 1] )
     D$AB[i] = sum( G[, 2] )
     D$BB[i] = sum( G[, 3] )
@@ -155,7 +280,17 @@ for( i in 1:10) {
     D$variant1[i] = info.variant1( G )
     D$variant2[i] = info.variant2( G )
     D$strange[i] = info.strange( G )
+
+    D$original.flipped[i] = info.original( G.flipped )
+    D$variant1.flipped[i] = info.variant1( G.flipped )
+    D$variant2.flipped[i] = info.variant2( G.flipped )
+    D$strange.flipped[i] = info.strange( G.flipped )
 }
+cat( i, 'of', nrow(D), '\n' )
+
+which( abs( D$original - D$original.flipped ) > 0.0001 )
+which( abs( D$variant1 - D$variant1.flipped ) > 0.0001 )
+which( abs( D$variant2 - D$variant2.flipped ) > 0.0001 )
 
 original.terms = sapply( 1:10, function( i ) {
     G = matrix( data = as.numeric( genotypes[i, 6:ncol( genotypes) ]), ncol = 3, byrow = T )
