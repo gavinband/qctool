369407298cf13d1dc48a08e1a34acfe93972f1c2
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-02-21 23:04:01 +0000

    Add configurability to make-qctool-static.sh
    FossilOrigin-Name: 3956ad28f112008669838a3d901789582762c288d2436943c8274c79f697ac10

--- make-qctool-static.sh
+++ make-qctool-static.sh
@@ -15,4 +15,9 @@ echo "I have it as \"$BOOST_PREFIX\"."
 
 g++ -O3 -o $TARGET src/*.cpp string_utils/src/*.cpp genfile/src/*.cpp statfile/src/*.cpp appcontext/src/*.cpp fputils/src/*.cpp worker/src/*.cpp $BOOST_PREFIX/lib/libboost_iostreams-gcc43-mt-1_44.a $BOOST_PREFIX/lib/libboost_filesystem-gcc43-mt-1_44.a /usr/lib/libz.a -static-libgcc -static -I include/ -I string_utils/include/ -I genfile/include/ -I statfile/include/ -I appcontext/include/ -I fputils/include/ -I worker/include/ -I $BOOST_PREFIX/include/boost-1_44/ /usr/lib/libz.a -I build/release/statfile/ -DHAVE_ZLIB=1 -DHAVE_BOOST=1 -DHAVE_BOOST_FILESYSTEM=1 -I build/release/genfile/include/ apps/qctool.cpp $BOOST_PREFIX/lib/libboost_system-gcc43-mt-1_44.a $BOOST_PREFIX/lib/libboost_thread-gcc43-mt-1_44.a -lpthread
 
+mv $TARGET /tmp/qctool
+chmod ugo+rwx /tmp/qctool
+gzip /tmp/qctool
+mv /tmp/qctool.gz ./$TARGET.gz
+
 
