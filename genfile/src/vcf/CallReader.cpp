066a049ca94a3196f2180bd6d52269bd0cbfd9bc
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-03-21 23:45:17 +0000

    genfile::vcf::CallReader: fixes for test_vcf_call_reader.
    FossilOrigin-Name: c6139d3750f899b8586f04979126b133bb08a9cc770d68ba8dcbb3d13ff2e844

--- genfile/src/vcf/CallReader.cpp
+++ genfile/src/vcf/CallReader.cpp
@@ -4,6 +4,43 @@
 
 namespace genfile {
 	namespace vcf {
+		namespace impl {
+			std::vector< std::string > parse_format( std::string const& format ) {
+				std::vector< std::string > result = string_utils::split( format, ":" ) ;
+				std::map< std::string, std::size_t > elt_counts ;
+				for( std::size_t i = 0; i < result.size(); ++i ) {
+					if( ++elt_counts[ result[i] ] > 1 ) {
+						throw BadArgumentError( "genfile::vcf::impl::parse_format()", "format = \"" + format + "\"" ) ;
+					}
+				}
+				if( result.empty() || result[0] != "GT" ) {
+					throw BadArgumentError( "genfile::vcf::impl::parse_format()", "format = \"" + format + "\"" ) ;
+					
+				}
+				return result ;
+			}
+
+			// Return a vector of pointers to VCFEntryType objects in the supplied map,
+			// with the ith VCFEntryType appropriate for the ith format element.
+			std::vector< VCFEntryType const* > get_entries_by_position(
+				std::vector< std::string > const& format_elts,
+				boost::ptr_map< std::string, VCFEntryType > const& entry_types
+			)
+			{
+				typedef boost::ptr_map< std::string, VCFEntryType >::const_iterator TypeIterator ;
+				std::vector< VCFEntryType const* > result( format_elts.size() ) ;
+				for( std::size_t i = 0; i < format_elts.size(); ++i ) {
+					TypeIterator where = entry_types.find( format_elts[i] ) ;
+					if( where == entry_types.end() ) {
+						throw BadArgumentError( "genfile::vcf::impl::get_entries_in_format()", "format_elts" ) ;
+					}
+					result[i] = where->second ;
+				}
+				return result ;
+			}
+
+			
+		}
 		CallReader::CallReader(
 			std::size_t number_of_alleles,
 			std::string const& format,
@@ -11,14 +48,15 @@ namespace genfile {
 			boost::ptr_map< std::string, VCFEntryType > const& entry_types
 		):
 			m_number_of_alleles( number_of_alleles ),
-			m_format_elts( string_utils::split( format, ":" )),
+			m_format_elts( impl::parse_format( format ) ),
 			m_data( data ),
 			m_entry_types( entry_types ),
-			m_entries_by_position( get_entries_by_position( m_format_elts, entry_types ))
+			m_entries_by_position( impl::get_entries_by_position( m_format_elts, entry_types ))
 		{
-			if( m_format_elts.empty() || m_format_elts[0] != "GT" ) {
-				throw BadArgumentError( "genfile::vcf::CallReader::CallReader()", "format = \"" + format + "\"" ) ;
+			if( m_number_of_alleles == 0 ) {
+				throw BadArgumentError( "genfile::vcf::CallReader::CallReader()", "number_of_alleles = " + string_utils::to_string( number_of_alleles ) ) ;
 			}
+
 			m_genotype_entry_type.reset(
 			 	new GenotypeCallVCFEntryType(
 					SimpleType::UniquePtr(
@@ -26,25 +64,6 @@ namespace genfile {
 					)
 				)
 			) ;
-		}
-
-		// Return a vector of pointers to VCFEntryType objects in the supplied map,
-		// with the ith VCFEntryType appropriate for the ith format element.
-		std::vector< VCFEntryType const* > CallReader::get_entries_by_position(
-			std::vector< std::string > const& format_elts,
-			boost::ptr_map< std::string, VCFEntryType > const& entry_types
-		) const
-		{
-			typedef boost::ptr_map< std::string, VCFEntryType >::const_iterator TypeIterator ;
-			std::vector< VCFEntryType const* > result( format_elts.size() ) ;
-			for( std::size_t i = 0; i < format_elts.size(); ++i ) {
-				TypeIterator where = entry_types.find( format_elts[i] ) ;
-				if( where == entry_types.end() ) {
-					throw BadArgumentError( "genfile::vcf::CallReader::get_entries_in_format()", "format_elts" ) ;
-				}
-				result[i] = where->second ;
-			}
-			return result ;
 		}
 
 		CallReader& CallReader::operator()( std::string const& spec, Setter setter ) {
@@ -58,9 +77,7 @@ namespace genfile {
 		}
 
 		CallReader::~CallReader() {
-			if( m_setters.size() > 0 ) {
-				set_values( string_utils::slice( m_data ).split( "\t" ), m_setters ) ;
-			}
+			set_values( string_utils::slice( m_data ).split( "\t" ), m_setters ) ;
 		}
 
 		void CallReader::set_values( std::vector< string_utils::slice > const& elts, Setters const& setters ) const {
@@ -76,11 +93,11 @@ namespace genfile {
 			}
 			
 			// First read the genotype calls, which must be present.
-			std::vector< Entry > genotypes = m_genotype_entry_type->parse( components[0], m_number_of_alleles ) ;
-			std::size_t const ploidy = genotypes.size() ;
-			
-			Setters::const_iterator i = m_setters.begin(), end_i = m_setters.end() ;
 			try {
+				std::vector< Entry > genotypes = m_genotype_entry_type->parse( components[0], m_number_of_alleles ) ;
+				std::size_t const ploidy = genotypes.size() ;
+			
+				Setters::const_iterator i = m_setters.begin(), end_i = m_setters.end() ;
 				for( ; i != end_i; ++i ) {
 					std::size_t element_pos = i->first ;
 					if( element_pos >= components.size() ) {
