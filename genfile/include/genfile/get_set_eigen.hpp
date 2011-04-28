8d44f768cda409d9f8b42d3d8964617c30942219
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-04-28 14:49:02 +0000

    added get_set_eigen from snptest
    FossilOrigin-Name: 8223a32dc8559c87820330687d19be9cf741a0a02d28c32002343292f52c0abd

---
+++ genfile/include/genfile/get_set_eigen.hpp
@@ -0,0 +1,20 @@
+#ifndef GENFILE_GET_SET_EIGEN_HPP
+#define GENFILE_GET_SET_EIGEN_HPP
+
+#ifdef HAVE_EIGEN
+#include "Eigen/Core"
+#include "genfile/get_set.hpp"
+
+namespace genfile {
+	template<>
+	struct GenotypeSetter< Eigen::MatrixXd >
+	{
+		GenotypeSetter( Eigen::MatrixXd& matrix ) ;
+		void operator()( std::size_t i, double, double, double ) const ;	
+		private:
+			Eigen::MatrixXd& m_matrix ;
+	} ;
+}
+#endif
+
+#endif
---
+++ genfile/src/get_set_eigen.cpp
@@ -0,0 +1,18 @@
+#if HAVE_EIGEN
+#include "genfile/get_set_eigen.hpp"
+
+namespace genfile {
+	GenotypeSetter< Eigen::MatrixXd >::GenotypeSetter( Eigen::MatrixXd& matrix ):
+		m_matrix( matrix )
+	{}
+	
+	void GenotypeSetter< Eigen::MatrixXd >::operator()( std::size_t i, double AA, double AB, double BB ) const {
+		assert( i < static_cast< std::size_t >( m_matrix.rows() ) ) ;
+		m_matrix( i, 0 ) = AA ;
+		m_matrix( i, 1 ) = AB ;
+		m_matrix( i, 2 ) = BB ;
+	}
+}
+
+#endif
+
