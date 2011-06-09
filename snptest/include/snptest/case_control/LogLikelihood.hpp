71619c81c38ea58e117049649ee533f63476dc03
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-06-09 12:52:02 +0000

    added LogLikelihood.hpp, needed by earlier commit.
    FossilOrigin-Name: dbb5174d1379e111a25546f3d1763f8d93fd612432dd4320743778c2cbc6ab42

---
+++ snptest/include/snptest/case_control/LogLikelihood.hpp
@@ -0,0 +1,68 @@
+#ifndef SNPTEST_CASE_CONTROL_LOG_LIKELIHOOD_HPP
+#define SNPTEST_CASE_CONTROL_LOG_LIKELIHOOD_HPP
+
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include "Eigen/Core"
+#include "snptest/FinitelySupportedFunctionSet.hpp"
+
+namespace snptest {
+	namespace case_control {
+		struct LogLikelihood: public boost::noncopyable
+		{
+		public:
+			typedef Eigen::VectorXd Point ;
+			typedef Eigen::VectorXd Vector ;
+			typedef Eigen::MatrixXd Matrix ;
+			
+			LogLikelihood(
+				Vector const& phenotypes,
+				FinitelySupportedFunctionSet const& genotypes
+			) ;
+
+			LogLikelihood(
+				Vector const& phenotypes,
+				FinitelySupportedFunctionSet const& genotypes,
+				std::vector< std::size_t > const& included_samples
+			) ;
+
+			void evaluate_at( Point const& parameters ) ;
+			double get_value_of_function() const ;
+			Vector get_value_of_first_derivative() const ;
+			Matrix get_value_of_second_derivative() const ;
+
+		private:
+			Vector const& m_phenotypes ;
+			FinitelySupportedFunctionSet const& m_genotypes ;
+			std::vector< Vector > m_design_matrices ;
+			std::vector< std::size_t > m_included_samples ;
+			Matrix m_p_g_thetas ;
+			Matrix m_coefficients ;
+
+		private:
+			std::vector< Vector > calculate_design_matrices( Vector const& genotype_levels ) const ;
+			virtual double calculate_p_g_theta( Vector const& parameters, double genotype, double phenotype ) const = 0 ;
+			Matrix calculate_p_g_thetas( Vector const& parameters, std::vector< Vector > const& design_matrices ) const ;
+			Matrix calculate_coefficients( Matrix const& genotypes, Vector const& phenotypes, Matrix const& p_g_thetas ) const ;
+			double calculate_function(
+				Vector const& phenotypes,
+				Matrix const& genotypes,
+				Matrix const& p_g_thetas
+			) const ;
+			Vector calculate_first_derivative(
+				Vector const& phenotypes,
+				Matrix const& p_g_thetas,
+				Matrix const& zs,
+				std::vector< Vector > const& design_matrices
+			) const ;
+			Matrix calculate_second_derivative(
+				Vector const& phenotypes,
+				Matrix const& p_g_thetas,
+				Matrix const& zs,
+				std::vector< Vector > const& design_matrices
+			) const ;
+		} ;
+	}
+}
+
+#endif
