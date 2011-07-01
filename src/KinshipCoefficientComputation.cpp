4cf9dc195244d478c352fa7363548dd117984ce3
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-07-01 09:57:19 +0000

    KinshipCoefficientComputation: fix to only use non-missing SNPs in denominator.  Also, ignore SNPs that are fixed in cohort.
    FossilOrigin-Name: 9cbccf4b1c5db5d3d2bf8063800d5e2e7a4d6dd5dc93926842f5d314fb8126ea

--- src/KinshipCoefficientComputation.cpp
+++ src/KinshipCoefficientComputation.cpp
@@ -27,54 +27,56 @@ double KinshipCoefficientComputation::operator()(
 	double non_missing_count = 0.0 ;
 	double result = 0.0 ;
 	for( std::size_t snp_i = 0; snp_i < genotypes.size(); ++snp_i ) {
-		std::size_t
-			genotype1 = std::numeric_limits< std::size_t >::max(),
-			genotype2 = std::numeric_limits< std::size_t >::max()
-		;
+		if( m_allele_frequencies[ snp_i] > 0.0 && m_allele_frequencies[ snp_i] < 1.0 ) {
+			std::size_t
+				genotype1 = std::numeric_limits< std::size_t >::max(),
+				genotype2 = std::numeric_limits< std::size_t >::max()
+			;
 
-		for( std::size_t g = 0; g < 3; ++g ) {
-			if( genotypes[snp_i]( sample1, g ) >= m_threshhold ) {
-				genotype1 = g ;
-				break ;
-			}
-		}
-
-		for( std::size_t g = 0; g < 3; ++g ) {
-			if( genotypes[snp_i]( sample2, g ) >= m_threshhold ) {
-				genotype2 = g ;
-				break ;
-			}
-		}
-
-		if( genotype1 < 3 && genotype2 < 3 ) {
-			++non_missing_count ;
-			if( sample1 != sample2 ) {
-				// Relationship between distinct individuals.
-				double twice_allele_frequency = 2.0 * m_allele_frequencies[ snp_i ] ;
-				result += ( genotype1 - twice_allele_frequency ) * ( genotype2 - twice_allele_frequency )
-					/ ( 2.0 * twice_allele_frequency * ( 1.0 - m_allele_frequencies[ snp_i ] ) ) ;
-			}
-			else {
-				// Relationship of individual with itself.
-				double Fhat ;
-				switch( genotype1 ) {
-					case 0:
-						Fhat = m_allele_frequencies[ snp_i ] / ( 1.0 - m_allele_frequencies[ snp_i ] ) ;
+			for( std::size_t g = 0; g < 3; ++g ) {
+				if( genotypes[snp_i]( sample1, g ) >= m_threshhold ) {
+					genotype1 = g ;
 					break ;
-					case 1:
-						Fhat = -1 ;
-					break ;
-					case 2:
-						Fhat = (1.0 - m_allele_frequencies[ snp_i ] ) / ( m_allele_frequencies[ snp_i ] ) ;
-					break ;
-					default:
-						assert(0) ;
 				}
-				result += 1.0 + Fhat ;
+			}
+
+			for( std::size_t g = 0; g < 3; ++g ) {
+				if( genotypes[snp_i]( sample2, g ) >= m_threshhold ) {
+					genotype2 = g ;
+					break ;
+				}
+			}
+
+			if( genotype1 < 3 && genotype2 < 3 ) {
+				++non_missing_count ;
+				if( sample1 != sample2 ) {
+					// Relationship between distinct individuals.
+					double twice_allele_frequency = 2.0 * m_allele_frequencies[ snp_i ] ;
+					result += ( genotype1 - twice_allele_frequency ) * ( genotype2 - twice_allele_frequency )
+						/ ( 2.0 * twice_allele_frequency * ( 1.0 - m_allele_frequencies[ snp_i ] ) ) ;
+				}
+				else {
+					// Relationship of individual with itself.
+					double Fhat ;
+					switch( genotype1 ) {
+						case 0:
+							Fhat = m_allele_frequencies[ snp_i ] / ( 1.0 - m_allele_frequencies[ snp_i ] ) ;
+						break ;
+						case 1:
+							Fhat = -1 ;
+						break ;
+						case 2:
+							Fhat = (1.0 - m_allele_frequencies[ snp_i ] ) / ( m_allele_frequencies[ snp_i ] ) ;
+						break ;
+						default:
+							assert(0) ;
+					}
+					result += 1.0 + Fhat ;
+				}
 			}
 		}
 	}
-	return result / genotypes.size() ;
+	return result / non_missing_count ;
 }
 	
 std::string KinshipCoefficientComputation::get_summary() const {
