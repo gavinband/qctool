c92a6405d2c577cae9a8696afc783e7a246b411e
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-07-19 16:28:53 +0000

    VCFFormatSNPDataSource: compilation fixes.
    FossilOrigin-Name: f1ed21bbc82ed6a48c378a86c4b7293ab323856685573e838ddd70e16a5d72fa

--- genfile/include/genfile/vcf/MetadataParser.hpp
+++ genfile/include/genfile/vcf/MetadataParser.hpp
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <string>
+#include <memory>
 #include <boost/noncopyable.hpp>
 
 namespace genfile {
--- genfile/include/genfile/vcf/TrivialMetadataParser.hpp
+++ genfile/include/genfile/vcf/TrivialMetadataParser.hpp
@@ -1,6 +1,7 @@
 #ifndef GENFILE_VCF_TRIVIAL_METADATA_PARSER_HPP
 #define GENFILE_VCF_TRIVIAL_METADATA_PARSER_HPP
 
+
 #include "genfile/vcf/MetadataParser.hpp"
 
 namespace genfile {
@@ -10,12 +11,12 @@ namespace genfile {
 			TrivialMetadataParser( std::string const& filename ) ;
 			TrivialMetadataParser( std::string const& spec, std::istream& stream ) ;
 
-			Metadata const& get_metadata() const { return Metadata() ; }
+			Metadata const& get_metadata() const { return m_metadata ; }
 			std::size_t get_number_of_lines() const { return m_number_of_lines ; }
 		private:
 			std::string const m_spec ;
 			std::size_t m_number_of_lines ;
-
+			Metadata const m_metadata ;
 			// Read through lines until either a line not starting with ##, or EOF is found.
 			void setup( std::istream& stream ) ;
 			bool read_metadata_line( std::istream& in, std::size_t line_number ) const ;
