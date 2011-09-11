5666050a5313c76032d7169b877b8a97b92e9f3c
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2011-09-11 21:53:58 +0000

    Split NormalClusterFitter into its own file.
    FossilOrigin-Name: 66694ecaf38c700e23b71bb7d7773ec1c739c6666c2efcd4f9c3635f919e08c4

--- include/ClusterFitter.hpp
+++ include/ClusterFitter.hpp
@@ -17,40 +17,5 @@ struct ClusterFitter: public genfile::SNPDataSourceProcessor::Callback
 	static UniquePtr create( appcontext::OptionProcessor const& options, genfile::SNPDataSourceProcessor::Callback* output = 0 ) ;
 } ;
 
-struct NormalClusterFitter: public ClusterFitter
-{
-	NormalClusterFitter( appcontext::OptionProcessor const& options ) ;
-	void begin_processing_snps( std::size_t number_of_samples, std::size_t number_of_snps ) ;
-	void processed_snp( genfile::SNPIdentifyingData const& id_data, genfile::VariantDataReader& genotypes ) ;
-	void end_processing_snps() ;
-private:
-	appcontext::OptionProcessor const& m_options ;
-	std::string const m_filename ;
-	std::auto_ptr< std::ostream > const m_file ;
-	std::vector< std::pair< std::string, std::string > > const m_spec ;
-	double const m_call_threshhold ;
-	std::size_t m_number_of_samples ;
-	std::size_t m_number_of_snps ;
-
-	void get_cluster_fit(
-		std::vector< genfile::VariantEntry > const& genotypes,
-		Eigen::MatrixXd const& intensities,
-		std::vector< Eigen::Vector2d >& means,
-		std::vector< Eigen::Matrix2d >& variances,
-		std::vector< std::size_t >& non_missing_counts
-	) ;
-
-	void write_output_columns(
-		std::vector< std::pair< std::string, std::string > > const& spec
-	) const ;
-
-	void write_id_data( genfile::SNPIdentifyingData const& id_data ) const ;
-
-	void write_cluster_fit(
-		std::vector< Eigen::Vector2d > const& means,
-		std::vector< Eigen::Matrix2d > const& variances,
-		std::vector< std::size_t > const& non_missing_counts
-	) const ;
-} ;
 
 #endif
---
+++ include/NormalClusterFitter.hpp
@@ -0,0 +1,48 @@
+#ifndef QCTOOL_NORMAL_CLUSTER_FITTER_HPP
+#define QCTOOL_NORMAL_CLUSTER_FITTER_HPP
+
+#include <memory>
+#include <vector>
+#include <utility>
+#include <string>
+#include "Eigen/Core"
+#include "genfile/SNPDataSourceProcessor.hpp"
+#include "ClusterFitter.hpp"
+
+struct NormalClusterFitter: public ClusterFitter
+{
+	NormalClusterFitter( appcontext::OptionProcessor const& options ) ;
+	void begin_processing_snps( std::size_t number_of_samples, std::size_t number_of_snps ) ;
+	void processed_snp( genfile::SNPIdentifyingData const& id_data, genfile::VariantDataReader& genotypes ) ;
+	void end_processing_snps() ;
+private:
+	appcontext::OptionProcessor const& m_options ;
+	std::string const m_filename ;
+	std::auto_ptr< std::ostream > const m_file ;
+	std::vector< std::pair< std::string, std::string > > const m_spec ;
+	double const m_call_threshhold ;
+	std::size_t m_number_of_samples ;
+	std::size_t m_number_of_snps ;
+
+	void get_cluster_fit(
+		std::vector< genfile::VariantEntry > const& genotypes,
+		Eigen::MatrixXd const& intensities,
+		std::vector< Eigen::Vector2d >& means,
+		std::vector< Eigen::Matrix2d >& variances,
+		std::vector< std::size_t >& non_missing_counts
+	) ;
+
+	void write_output_columns(
+		std::vector< std::pair< std::string, std::string > > const& spec
+	) const ;
+
+	void write_id_data( genfile::SNPIdentifyingData const& id_data ) const ;
+
+	void write_cluster_fit(
+		std::vector< Eigen::Vector2d > const& means,
+		std::vector< Eigen::Matrix2d > const& variances,
+		std::vector< std::size_t > const& non_missing_counts
+	) const ;
+} ;
+
+#endif
