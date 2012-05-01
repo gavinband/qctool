cf1c3c4a11df8c0ac9bf71f8d1037e2151fe17ab
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2012-05-01 16:18:01 +0000

    Added metro/ which is incomplete.
    FossilOrigin-Name: d74328755cbb9f93c90c09d6f1449effb47d8899874515eb5c7a44913d9853f8

---
+++ metro/include/metro/likelihood/Multinomial.hpp
@@ -0,0 +1,72 @@
+
+//          Copyright Gavin Band 2008 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef METRO_LIKELIHOOD_MULTINOMIAL_HPP
+#define METRO_LIKELIHOOD_MULTINOMIAL_HPP
+
+#include <cmath>
+#include "metro/LogLikelihood.hpp"
+
+namespace metro {
+	namespace likelihood {
+		template< typename Scalar, typename Vector, typename Matrix >
+		struct Multinomial: public metro::LogLikelihood< Scalar, Vector, Matrix > {
+			Multinomial( Vector const& counts ):
+				m_counts( counts ),
+				m_parameters( Vector::Zero( counts.size() ) )
+			{
+			}
+
+			Multinomial( Vector const& counts, Vector const& parameters ):
+				m_counts( counts ),
+				m_parameters( parameters )
+			{
+				assert( parameters.size() == counts.size() ) ;
+			}
+
+			void evaluate_at( Vector const& parameters ) {
+				assert( parameters.size() == m_counts.size() ) ;
+				m_parameters = parameters ;
+			}
+
+			double get_value_of_function() const {
+				double result = 0.0 ;
+				for( int i = 0; i < m_counts.size(); ++i ) {
+					if( m_counts(i) > 0.0 ) {
+						result += m_counts(i) * std::log( m_parameters( i )) ;
+					}
+				}
+				return result ;
+			}
+
+			Vector get_value_of_first_derivative() const {
+				Vector result = Vector::Zero( m_counts.size() ) ;
+				for( int i = 0; i < result.size(); ++i ) {
+					if( m_counts(i) > 0.0 ) {
+						result(i) = m_counts(i) / m_parameters(i) ;
+					}
+				}
+				return result ;
+			}
+
+			Matrix get_value_of_second_derivative() const {
+				Matrix result = Matrix::Zero( m_counts.size(), m_counts.size() ) ;
+				for( int i = 0; i < result.size(); ++i ) {
+					if( m_counts(i) != 0.0 ) {
+						result( i, i ) = -m_counts(i) / ( m_parameters(i) * m_parameters(i) ) ;
+					}
+				}
+				return result ;
+			}
+
+		private:
+			Vector const m_counts ;
+			Vector m_parameters ;
+		} ;
+	}
+}
+
+#endif
