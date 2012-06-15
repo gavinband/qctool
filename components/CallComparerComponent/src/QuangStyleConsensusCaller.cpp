f63477600b7499380fab4b12967475f5672340a2
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2012-06-15 10:35:57 +0000

    QuangStyleConsensusCaller: bug fix.
    FossilOrigin-Name: 41bf7be6eaed5e89530d59ba65b6340e235eb004d3874fdd9f5e4357ab5557b8

--- components/CallComparerComponent/src/QuangStyleConsensusCaller.cpp
+++ components/CallComparerComponent/src/QuangStyleConsensusCaller.cpp
@@ -27,8 +27,6 @@ void QuangStyleConsensusCaller::processed_snp( genfile::SNPIdentifyingData const
 	std::map< std::string, std::vector< genfile::VariantEntry > > info ;
 	std::size_t chosen_call = 0 ;
 
-	m_result_probs.setZero( m_number_of_samples, 3 ) ;
-
 	if( call_names.size() > 0 ) {
 		m_consensus_counts.setConstant( m_number_of_samples, call_names.size() ) ;
 		m_result_calls.setZero( m_number_of_samples ) ;
@@ -39,7 +37,7 @@ void QuangStyleConsensusCaller::processed_snp( genfile::SNPIdentifyingData const
 #endif
 			// get hard-called genotypes, 0 = missing, 1, 2, 3 as levels.
 			{
-				genfile::vcf::ThreshholdingGenotypeSetter< Eigen::VectorXd > setter( m_genotypes, 0, 1, 2, 3 ) ;
+				genfile::vcf::ThreshholdingGenotypeSetter< Eigen::VectorXd > setter( m_genotypes, m_call_threshhold, 0, 1, 2, 3 ) ;
 				data_reader.get( call_names[i], setter ) ;
 			}
 			assert( std::size_t( m_genotypes.size() ) == m_number_of_samples ) ;
@@ -87,9 +85,10 @@ void QuangStyleConsensusCaller::processed_snp( genfile::SNPIdentifyingData const
 		info[ "missing_calls" ].push_back( ( m_result_calls.array() == 0.0 ).cast< int >().sum() ) ;
 
 		// Convert to probabilities.
+		m_result_probs.setZero( m_number_of_samples, 3 ) ;
 		for( std::size_t i = 0; i < m_number_of_samples; ++i ) {
 			if( m_result_calls( i ) > 0.0 ) {
-				m_result_probs( i, m_genotypes( i ) - 1 ) = 1 ;
+				m_result_probs( i, m_result_calls( i ) - 1 ) = 1 ;
 			}
 		}
 
