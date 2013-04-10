0839a8b1b6ded5ce6149c5204032b55f04c475e1
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-04-10 20:18:35 +0000

    Added missing file.
    FossilOrigin-Name: 5c0ff56250bd570d1baf6ae0e2bc7c84872f180612ffb7a2ed7ecbfdb60a147a

---
+++ qcdb/include/qcdb/StorageOptions.hpp
@@ -0,0 +1,15 @@
+
+//          Copyright Gavin Band 2008 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef QCTOOL_QCDB_STORAGE_OPTIONS_HPP
+#define QCTOOL_QCDB_STORAGE_OPTIONS_HPP
+
+namespace qcdb {
+	enum StorageOptions { eCreateIndices = 1 } ;
+}
+
+#endif
+
