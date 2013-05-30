8e6cb4d130b20f8d364e3723cee5d3c76421eab8
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-05-30 08:54:41 +0000

    bingwa: minor tweaks.
    FossilOrigin-Name: 53009054b300e27d74c62cb2334b136cb1b6989af6c3bb855cac69c71815eb75

--- apps/bingwa.cpp
+++ apps/bingwa.cpp
@@ -1332,7 +1332,7 @@ public:
 					// to be fed data before the mean is computed.
 					{
 						ValueAccumulator::UniquePtr mean_bf(
-							new ValueAccumulator( "mean_bf" )
+							new ValueAccumulator( "ApproximateBayesianMetaAnalysis/mean_bf" )
 						) ;
 
 						std::map< std::string, Eigen::MatrixXd >::const_iterator i = priors.begin() ;
@@ -1505,8 +1505,8 @@ public:
 			Eigen::MatrixXd prior( number_of_cohorts, number_of_cohorts ) ;
 			prior.setZero() ;
 			std::vector< int > cohort_indices( cohorts.size() ) ;
-			for( std::size_t i = 0; i < cohorts.size(); ++i ) {
-				std::vector< std::string >::const_iterator const cohort_i = std::find( cohort_names.begin(), cohort_names.end(), cohorts[i] ) ;
+			for( std::size_t k = 0; k < cohorts.size(); ++k ) {
+				std::vector< std::string >::const_iterator const cohort_i = std::find( cohort_names.begin(), cohort_names.end(), cohorts[k] ) ;
 				if( cohort_i == cohort_names.end() ) {
 					throw genfile::BadArgumentError(
 						"BingwaProcessor::get_group_priors()",
@@ -1514,7 +1514,7 @@ public:
 						"Unrecognised cohort name, possible names are \"" + join( cohort_names, "\", \"" ) + "\""
 					) ;
 				}
-				cohort_indices[i] = int( cohort_i - cohort_names.begin() ) ;
+				cohort_indices[k] = int( cohort_i - cohort_names.begin() ) ;
 			}
 			
 			for( std::size_t j1 = 0; j1 < cohorts.size(); ++j1 ) {
