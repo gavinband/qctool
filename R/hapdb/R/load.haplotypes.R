2a5a498a7c4be0cb31acdafb75604656de9e3876
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-11-18 08:29:45 +0000

    Fixes to hapdb
    FossilOrigin-Name: cffa9eebc11ae53544d34914443718c3fe34a9e24b9fa185d81ea655834ace3f

--- R/hapdb/R/load.haplotypes.R
+++ R/hapdb/R/load.haplotypes.R
@@ -1,11 +1,55 @@
 load.haplotypes <-
-function( db, rsid, analysis = NULL ) {
-    if( is.null( analysis ) ) {
-        sql = sprintf( "SELECT analysis_id, E.name AS analysis, V.id AS variant_id, chromosome, position, rsid, alleleA, alleleB, H.N AS N, H.data FROM Haplotype H INNER JOIN Entity E ON E.id == H.analysis_id INNER JOIN Variant V ON V.id == H.variant_id AND V.rsid == '%s'", rsid )
-    } else {
-        sql = sprintf( "SELECT analysis_id, E.name AS analysis, V.id AS variant_id, chromosome, position, rsid, alleleA, alleleB, H.N AS N, H.data FROM Haplotype H INNER JOIN Entity E ON E.id == H.analysis_id INNER JOIN Variant V ON V.id == H.variant_id AND E.name == '%s' AND V.rsid == '%s'", analysis, rsid )
-    }
+function( hapdb, chromosome, rsid = NULL, range = NULL, analysis = NULL ) {
+    require( RSQLite )
     require( Rcompression )
+    sql = paste(
+        "SELECT analysis_id, E.name AS analysis, V.id AS variant_id, chromosome, position, rsid, alleleA, alleleB, H.N AS N, H.data",
+        "FROM Haplotype H",
+        "INNER JOIN Entity E ON E.id == H.analysis_id",
+        "INNER JOIN Variant V ON V.id == H.variant_id",
+        "WHERE 1 == 1",
+        sep = " "
+    )
+    
+    if( length( unique( hapdb$samples$analysis_id ) ) > 0 ) {
+        if( is.null( analysis )) {
+            stop( "This hapdb file has more than one analysis, please specify one." ) ;
+        } else {
+            analysis_id = hapdb$samples$analysis_id[ match( analysis, hapdb$samples$analysis ) ]
+            if( is.na( analysis_id )) {
+                stop( paste( "Analysis \"", analysis, "\" was not found in the hapdb file.", sep = "" ) ) ;
+            }
+            sql = paste(
+                sql,
+                sprintf( "AND analysis_id == %d", analysis_id ),
+                sep = " "
+            ) ;
+        }
+    }
+    
+    if( !is.null( chromosome ) ) {
+        sql = paste(
+            sql,
+            sprintf( "AND chromosome == '%s'", as.character( chromosome ) ),
+            sep = " "
+        )
+    }
+    if( !is.null( range ) ) {
+        sql = paste(
+            sql,
+            sprintf( "AND position BETWEEN %d AND %d", as.integer( range[1] ), as.integer( range[2] ) ),
+            sep = " "
+        )
+    }
+    if( !is.null( rsid ) ) {
+        sql = paste(
+            sql,
+            "AND rsid IN (",
+            paste( sprintf( '"%s"', rsid ), collapse = ',' ),
+            sep = " "
+        )
+    }
+
     D = dbGetQuery( db, sql )
 
     result = list(
@@ -16,7 +60,7 @@ function( db, rsid, analysis = NULL ) {
     for( i in 1:nrow(D) ) {
         compressed_data = unlist( D$data[i] )
         uncompressed_data = uncompress( compressed_data, asText = FALSE )
-        result$data[i,] = parse_haplotypes( uncompressed_data[10:length(uncompressed_data)], D$N[i] ) ;
+        result$data[i,] = parse_haplotypes( uncompressed_data, D$N[i] )
     }
     return( result )
 }
--- R/hapdb/R/open.hapdb.R
+++ R/hapdb/R/open.hapdb.R
@@ -2,13 +2,22 @@ open.hapdb <-
 function( filename, get.variants = FALSE ) {
     db = dbConnect( dbDriver( "SQLite" ), filename ) ;
     cat( "open.hapdb(): loading samples and variants..." ) ;
-    samples = dbGetQuery( db, "SELECT * FROM Sample ORDER BY analysis_id, index_in_data" ) ;
+    samples = dbGetQuery(
+        db,
+        paste(
+            'SELECT S.analysis_id AS analysis_id, E.name AS analysis, sample, index_in_data AS "index"',
+            'FROM Sample S',
+            'INNER JOIN Entity E ON E.id == S.analysis_id',
+            'ORDER BY analysis_id, index_in_data',
+            sep = " "
+        )
+    )
 	result = list(
 		db = db,
 		samples = samples
 	) ;
 	if( get.variants ) {
-		result$variants = dbGetQuery( db, "SELECT * FROM Variant" ) ;
+		result$variants = get.variants( result ) ;
 	}
     return( result ) ;
 }
