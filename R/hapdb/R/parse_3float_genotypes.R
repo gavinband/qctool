ec3f6ab33e90f683f1491532fe943710181f8797
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-12-13 13:31:00 +0000

    In hapdb: deal with missing genotypes (stored as -1).
    FossilOrigin-Name: dfe5a96eaa22dfe767631d8dd525259adb96e17de45dd72712c73d3aef726f33

--- R/hapdb/R/parse_3float_genotypes.R
+++ R/hapdb/R/parse_3float_genotypes.R
@@ -1,6 +1,7 @@
 parse_3float_genotypes <-
 function( data, N ) {
     stopifnot( length( data ) == N*3*4 ) ;
-    result = readBin( data, what = "numeric", size = 4 )
+    result = readBin( data, what = "numeric", size = 4, n = N*3 )
+    result[ which( result == -1 )] = NA
     return( result )
 }
