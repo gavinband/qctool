365ac90898bf47bd491b6cce19ced847d4652f75
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-12-17 11:42:48 +0000

    Fix parse_intensities.R
    FossilOrigin-Name: 7b09e0b9384e04e94b42f8b824480f81d62fecdcd8f0c006d86ee6ed174746be

--- R/hapdb/R/parse_intensities.R
+++ R/hapdb/R/parse_intensities.R
@@ -1,15 +1,19 @@
 parse_intensities <-
 function( data, N ) {
-    if(
-        ( data[1] == '73' && data[2] == 0 && data[3] == 6 && rawToChar( data[4:9] ) == '2float' ) # for backwards compatibility
-        || ( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:14] ) == 'floatarray' )
-    ) {
+    if( data[1] == '73' && data[2] == 0 && data[3] == 6 && rawToChar( data[4:9] ) == '2float' ) { # for backwards compatibility
         number_of_samples = readBin( data[11:14], what = "integer", endian = "big" )
         stopifnot( number_of_samples == N ) ;
         number_of_entries = readBin( data[16:17], what = "integer", endian = "big", size = 2 )
         stopifnot( number_of_entries == 2 ) ;
         result = parse_floats( data[18:length(data)], N, 2 ) ;
     }
+    else if( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:14] ) == 'floatarray' ) {
+        number_of_samples = readBin( data[15:18], what = "integer", endian = "big" )
+        stopifnot( number_of_samples == N ) ;
+        number_of_entries = readBin( data[20:21], what = "integer", endian = "big", size = 2 )
+        stopifnot( number_of_entries == 2 ) ;
+        result = parse_floats( data[12:length(data)], N, 2 ) ;
+    )
     else {
         stop() ;
     }
