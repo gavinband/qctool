c0a9b70220b075824fee0884d2270d1f410a4f20
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2013-12-17 11:53:34 +0000

    fix parse_genotypes and parse_intensities again.
    FossilOrigin-Name: 14a6da88afbf2adcc0fadfbf16972d8503a0a54226e68ac94ff8d188fdaae64c

--- R/hapdb/R/parse_genotypes.R
+++ R/hapdb/R/parse_genotypes.R
@@ -1,14 +1,18 @@
 parse_genotypes <-
 function( data, N ) {
-    if(
-        ( data[1] == '73' && data[2] == 0 && data[3] == 6 && rawToChar( data[4:9] ) == '3float' ) # for backwards compatibility
-        || ( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:14] ) == 'floatarray' )
-    ) {
+    if( data[1] == '73' && data[2] == 0 && data[3] == 6 && rawToChar( data[4:9] ) == '3float' ) { # for backward compatibility
         number_of_samples = readBin( data[11:14], what = "integer", endian = "big" )
         stopifnot( number_of_samples == N ) ;
         number_of_entries = readBin( data[16:17], what = "integer", endian = "big", size = 2 )
         stopifnot( number_of_entries == 3 ) ;
         result = parse_floats( data[18:length(data)], N, 3 ) ;
+    }
+    else if ( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:13] ) == 'floatarray' ) {
+        number_of_samples = readBin( data[15:18], what = "integer", endian = "big" )
+        stopifnot( number_of_samples == N ) ;
+        number_of_entries = readBin( data[20:21], what = "integer", endian = "big", size = 2 )
+        stopifnot( number_of_entries == 3 ) ;
+        result = parse_floats( data[22:length(data)], N, 3 ) ;
     }
     else {
         stop() ;
--- R/hapdb/R/parse_intensities.R
+++ R/hapdb/R/parse_intensities.R
@@ -7,12 +7,12 @@ function( data, N ) {
         stopifnot( number_of_entries == 2 ) ;
         result = parse_floats( data[18:length(data)], N, 2 ) ;
     }
-    else if( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:14] ) == 'floatarray' ) {
+    else if ( data[1] == '73' && data[2] == 0 && data[3] == 10 && rawToChar( data[4:13] ) == 'floatarray' ) {
         number_of_samples = readBin( data[15:18], what = "integer", endian = "big" )
         stopifnot( number_of_samples == N ) ;
         number_of_entries = readBin( data[20:21], what = "integer", endian = "big", size = 2 )
         stopifnot( number_of_entries == 2 ) ;
-        result = parse_floats( data[12:length(data)], N, 2 ) ;
+        result = parse_floats( data[22:length(data)], N, 2 ) ;
     }
     else {
         stop() ;
