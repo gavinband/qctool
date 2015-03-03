666e23912d5fec441246208d6537a65d7340ddd5
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2015-03-03 11:55:55 +0000

    RelatednessComponent: just use sample id_1, not both sample ids
    FossilOrigin-Name: b49dc67ee21627c53bdc48ce0cb882a70d9f64f2420831ff17e5b1fe99ff458b

--- components/RelatednessComponent/src/names.cpp
+++ components/RelatednessComponent/src/names.cpp
@@ -11,7 +11,7 @@
 
 namespace pca {
 	std::string get_concatenated_sample_ids( genfile::CohortIndividualSource const* samples, std::size_t i ) {
-		return samples->get_entry( i, "id_1" ).as< std::string >() + ":" + samples->get_entry( i, "id_2" ).as< std::string >() ;
+		return samples->get_entry( i, "id_1" ).as< std::string >() ;
 	}
 	
 	std::string string_and_number( std::string const& s, std::size_t i ) {
