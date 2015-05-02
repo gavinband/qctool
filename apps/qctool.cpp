ceb77560c464905badd72b6a3fc56b99acff34b1
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2015-05-02 21:58:27 +0000

    -bgen-suppress-sample-identifier-block => -bgen-omit-sample-identifier-block
    FossilOrigin-Name: a00bcb4dfc674687c3a3bab18c97bd1a4273241b7209d41ba7f170def12436e4

--- apps/qctool.cpp
+++ apps/qctool.cpp
@@ -398,7 +398,7 @@ public:
 				" in the BGEN header block." )
 			.set_takes_single_value()
 		;
-		options[ "-bgen-suppress-sample-identifier-block" ]
+		options[ "-bgen-omit-sample-identifier-block" ]
 			.set_description( "For use when outputting BGEN files only.  Tell QCTOOL to omit the sample identifier block.  By default"
 				" this is written whenever -s is specified." )
 		;
@@ -1910,7 +1910,7 @@ private:
 							if( m_options.check( "-bgen-free-data" )) {
 								bgen_sink->set_free_data( m_options.get< std::string >( "-bgen-free-data" ) ) ;
 							}
-							bgen_sink->set_write_sample_identifier_block( m_options.check( "-s" ) && ! m_options.check( "-bgen-suppress-sample-identifier-block" )) ;
+							bgen_sink->set_write_sample_identifier_block( m_options.check( "-s" ) && ! m_options.check( "-bgen-omit-sample-identifier-block" )) ;
 						}
 					}
 					if( m_options.check( "-sort" )) {
