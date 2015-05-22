56e9fd04e675e78a4c21fc3581d99d5ab1f2cf09
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2015-05-22 16:12:32 +0000

    DataSubset: initial work on new add() function
    FossilOrigin-Name: cb31ea979aa0a30eed8422d53fc2268c1c3be47cff6a91fcb342b4b0b7cc5f7e

--- metro/src/DataSubset.cpp
+++ metro/src/DataSubset.cpp
@@ -11,6 +11,40 @@
 
 namespace metro {
 
+#if 0
+	void DataSubset::add( DataRange const& other ) {
+		std::vector< DataRange > newRep ;
+		Rep::const_iterator i = m_rep.begin() ;
+		for(
+			;
+			i != m_rep.end() && i->end() < other.begin();
+			++i
+		) ;
+		std::copy( m_rep.begin(), i, std::back_inserter< newRep >() ) ;
+		int begin = other.begin() ;
+		int end = other.end() ;
+		for(
+			;
+			i != m_rep.end() && i->begin() < other.end();
+			++i
+		) {
+			begin = std::min( begin, i->begin() ) ;
+			end = std::max( end, i->end() ) ;
+		}
+
+
+
+		for( std::size_t i = 0; i < m_rep.size() && ; ++i ) {
+
+			if( m_rep[i].end() < m_rep[i].begin() || m_rep[i].begin() > m_rep[i].end() ) {
+				end = i ;
+			} else {
+				// join
+			}
+		}
+	}
+#endif
+	
 	void DataSubset::add( DataSubset const& other ) {
 		m_rep.insert( m_rep.end(), other.m_rep.begin(), other.m_rep.end() ) ;
 		std::sort( m_rep.begin(), m_rep.end() ) ;
