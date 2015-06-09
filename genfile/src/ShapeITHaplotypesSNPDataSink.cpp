69697e3fc9e34012222dd30614d6c641954acf94
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2015-06-09 09:14:40 +0000

    ShapeITHaplotypesSNPDataSink.cpp: fix to properly output haplotypes
    FossilOrigin-Name: 66871ae762dfc4bedb70f3c429d39e226ef3d77f2af6b7035efd781a29f1b09a

--- genfile/src/ShapeITHaplotypesSNPDataSink.cpp
+++ genfile/src/ShapeITHaplotypesSNPDataSink.cpp
@@ -71,16 +71,16 @@ namespace genfile {
 				}
 			}
 			void operator()( MissingValue const value ) {
-				m_data( m_sample_i * 2 + m_entry_i ) = -1 ;
+				m_data( m_sample_i * 2 + m_entry_i++ ) = -1 ;
 			}
 			void operator()( std::string& value ) {
 				assert(0) ;
 			}
 			void operator()( Integer const value ) {
-				m_data( m_sample_i * 2 + m_entry_i ) = value ;
+				m_data( m_sample_i * 2 + m_entry_i++ ) = value ;
 			}
 			void operator()( double const value ) {
-				m_data( m_sample_i * 2 + m_entry_i ) = value ;
+				m_data( m_sample_i * 2 + m_entry_i++ ) = value ;
 			}
 			
 			void write_to_stream( std::ostream& ostr ) const {
