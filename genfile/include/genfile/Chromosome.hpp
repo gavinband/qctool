c19e68512edd8381e6bf2d07b71dd59c5da035ac
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2016-07-08 08:47:47 +0000

    Chromosome: update to allow any chromosome identifier with custom sort order
    FossilOrigin-Name: 5a9d4ada76397bd8c0b49f31c4c2048f43458b6d7b3c0fe0b768f818826aad0e

--- genfile/include/genfile/Chromosome.hpp
+++ genfile/include/genfile/Chromosome.hpp
@@ -9,41 +9,66 @@
 
 #include <string>
 #include <iostream>
+#include <boost/noncopyable.hpp>
 #include <boost/optional.hpp>
+#include <boost/unordered_map.hpp>
 
 namespace genfile {
-	struct Genome {
-	} ;
+	struct Chromosome ;
+	namespace impl {
+		struct Genome: public boost::noncopyable {
+			virtual ~Genome() {} ;
+			virtual bool compare( Chromosome const& left, Chromosome const& right ) const = 0 ;
+		} ;
 
+		struct MapGenome: public Genome {
+			MapGenome( char const** strings, std::size_t N ) ;
+			~MapGenome() ;
+			bool compare( Chromosome const& left, Chromosome const& right ) const ;
+		private:
+			typedef boost::unordered_map< std::string, int > MapType ;
+			MapType m_map ;
+		} ;
+		
+		extern MapGenome human ;
+	}
+	
 	struct Chromosome
 	{
-		Chromosome():
+	public:
+		friend struct impl::MapGenome ;
+
+	public:
+		Chromosome( impl::Genome const& genome = impl::human ):
+			m_genome( &genome ),
 			m_repr()
 		{}
 
-		Chromosome( std::string const& chromosome_str ):
+		Chromosome( std::string const& chromosome_str, impl::Genome const& genome = impl::human ):
+			m_genome( &genome ),
 			m_repr( chromosome_str )
-		{}
+		{
+			if( *m_repr == "NA" ) {
+				m_repr = boost::optional< std::string >() ;
+			}
+		}
 
 		Chromosome( Chromosome const& other ):
+			m_genome( other.m_genome ),
 			m_repr( other.m_repr )
 		{}
 
 		Chromosome& operator=( Chromosome const& other ) {
+			m_genome = other.m_genome ;
 			m_repr = other.m_repr ;
 			return *this ;
 		}
 
-		Chromosome& operator=( std::string const& other ) {
-			m_repr = other ;
-			return *this ;
-		}
-		
 		bool operator==( std::string const& other ) const {
 			return m_repr == other ;
 		}
 
-		bool operator==( Chromosome other ) const {
+		bool operator==( Chromosome const& other ) const {
 			return m_repr == other.m_repr ;
 		}
 
@@ -51,24 +76,24 @@ namespace genfile {
 			return m_repr != other ;
 		}
 		
-		bool operator!=( Chromosome other ) const {
+		bool operator!=( Chromosome const& other ) const {
 			return m_repr != other.m_repr ;
 		}
 		
-		bool operator<=( Chromosome other ) const {
-			return m_repr <= other.m_repr ;
+		bool operator<=( Chromosome const& other ) const {
+			return !m_genome->compare( other, *this ) ;
 		}
 
-		bool operator>=( Chromosome other ) const {
-			return m_repr >= other.m_repr ;
+		bool operator>=( Chromosome const& other ) const {
+			return !m_genome->compare( *this, other ) ;
 		}
 
-		bool operator<( Chromosome other ) const {
-			return m_repr < other.m_repr ;
+		bool operator<( Chromosome const& other ) const {
+			return m_genome->compare( *this, other ) ;
 		}
 
-		bool operator>( Chromosome other ) const {
-			return m_repr > other.m_repr ;
+		bool operator>( Chromosome const& other ) const {
+			return m_genome->compare( other, *this ) ;
 		}
 		
 		bool is_sex_determining() const ;
@@ -78,6 +103,7 @@ namespace genfile {
 		operator std::string () const ;
 
 	private:
+		impl::Genome const* m_genome ;
 		boost::optional< std::string > m_repr ;
 	} ;
 	
--- genfile/src/Chromosome.cpp
+++ genfile/src/Chromosome.cpp
@@ -13,6 +13,67 @@
 #include "genfile/string_utils/string_utils.hpp"
 
 namespace genfile {
+	namespace impl {
+		namespace {
+			char const* humanChromosomes [] = {
+				"1", "01", "chr1",
+				"2", "02", "chr2",
+				"3", "03", "chr3",
+				"4", "04", "chr4",
+				"5", "05", "chr5",
+				"6", "06", "chr6",
+				"7", "07", "chr7",
+				"8", "08", "chr8",
+				"9", "09", "chr9",
+				"10", "chr10",
+				"11", "chr11",
+				"12", "chr12",
+				"13", "chr13",
+				"14", "chr14",
+				"15", "chr15",
+				"16", "chr16",
+				"17", "chr17",
+				"18", "chr18",
+				"19", "chr19",
+				"20", "chr20",
+				"21", "chr21",
+				"22", "chr22",
+				"X", "0X", "chrX", "23",
+				"Y", "0Y", "chrY",
+				"MT", "chrMT"
+			} ;
+		}
+		MapGenome human(
+			humanChromosomes,
+			3*9 + 13*2 + 4 + 3 + 2
+		) ;
+	}
+
+	namespace impl {
+		MapGenome::MapGenome( char const** strings, std::size_t N ) {
+			for( std::size_t i = 0; i < N; ++i ) {
+				m_map[ strings[i] ] = i ;
+			}
+		}
+
+		MapGenome::~MapGenome() {
+		}
+		
+		bool MapGenome::compare( Chromosome const& left, Chromosome const& right ) const {
+			bool const leftIsMissing = !left.m_repr ;
+			bool rightIsMissing = !right.m_repr ;
+			if( leftIsMissing || rightIsMissing ) {
+				return (!leftIsMissing) && rightIsMissing ;
+			}
+			MapType::const_iterator li = m_map.find( *left.m_repr ) ;
+			MapType::const_iterator ri = m_map.find( *right.m_repr ) ;
+			return (
+				( li != m_map.end() ) && (( ri == m_map.end() ) || ( li->second < ri->second ))
+				||
+				(( li == m_map.end() ) && (( ri == m_map.end() ) && (left.m_repr < right.m_repr)))
+			) ;		
+		}
+	}
 	std::ostream& operator<<( std::ostream& oStream, Chromosome const& chromosome ) {
 		return oStream << static_cast< std::string >( chromosome ) ;
 	}
