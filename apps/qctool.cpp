378231227e21319654113ff803499cb049bd69c1
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2017-04-20 11:10:23 +0000

    Make -map-id-data respect -snp-match-fields
    FossilOrigin-Name: 12c1fd4ef7a24c3f173a0b86c03ed58a09bda751447ece173e4b9f4d6fe7a08f

--- apps/qctool.cpp
+++ apps/qctool.cpp
@@ -1005,7 +1005,7 @@ private:
 	QCToolOptionMangler const m_mangled_options ;
 	appcontext::UIContext& m_ui_context ;
 
-	typedef std::map< genfile::VariantIdentifyingData, genfile::VariantIdentifyingData > SNPDictionary ;
+	typedef std::map< genfile::VariantIdentifyingData, genfile::VariantIdentifyingData, genfile::VariantIdentifyingData::CompareFields > SNPDictionary ;
 	std::auto_ptr< SNPDictionary > m_snp_dictionary ;
 
 	typedef genfile::StrandAligningSNPDataSource::StrandFlipSpec StrandFlipSpec ;
@@ -1640,7 +1640,7 @@ private:
 	}
 	
 	std::auto_ptr< SNPDictionary > load_snp_dictionary( std::string const& filename ) const {
-		std::auto_ptr< SNPDictionary > result( new SNPDictionary ) ;
+		std::auto_ptr< SNPDictionary > result( new SNPDictionary( genfile::VariantIdentifyingData::CompareFields( m_options.get_value< std::string >( "-snp-match-fields" ) ) )) ;
 		statfile::BuiltInTypeStatSource::UniquePtr source( 
 			statfile::BuiltInTypeStatSource::open(
 				genfile::wildcard::find_files_by_chromosome( filename )
