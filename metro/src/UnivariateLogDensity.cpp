f4e65b7bca2031af0d1dd0655c7201b79f18d17e
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk@noemail.net>
2019-01-10 15:32:52 +0000

    metro: add src for UnivariateLogDensity
    FossilOrigin-Name: 2e04fb72f82931ae1846508c08365f3310ffd2f3a5ca3c22ad1c30b51f74ffe8

---
+++ metro/src/UnivariateLogDensity.cpp
@@ -0,0 +1,13 @@
+
+//          Copyright Gavin Band 2008 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include "metro/UnivariateLogDensity.hpp"
+#include <memory>
+
+namespace metro {
+	UnivariateLogDensity::~UnivariateLogDensity() {}
+}
+
