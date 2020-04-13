d912de05e65a83b100f69b59150795426a848431
gav <gav@noemail.net>
2020-04-13 20:27:45 +0000

    Finish work on compilation with new waf version
    FossilOrigin-Name: 406a471f3443ed0f797373f145488c263a24fabb1688187638195d98624b330d

--- src/FileBackupCreator.cpp
+++ src/FileBackupCreator.cpp
@@ -8,8 +8,9 @@
 #include <string>
 #include <cstdio>
 #include "FileBackupCreator.hpp"
-#include "string_utils/string_utils.hpp"
+#include "genfile/string_utils/string_utils.hpp"
 #include <boost/filesystem.hpp>
+
 namespace BFS = boost::filesystem ;
 
 void FileBackupCreator::backup_file_if_necessary( std::string const& filename ) {
@@ -45,7 +46,7 @@ std::string ToNumberedFileBackupCreator::backup_file_if_necessary_impl( std::str
 	// Generate a list of numbered filenames whose numbers we'll increment.
 	// If we hit the limit set by m_max_number_of_backups, we delete the last of these.
 	while( BFS::exists( filename_stack.back() )) {
-		std::string next_backup = filename + ".~" + string_utils::to_string( filename_stack.size() ) ;
+		std::string next_backup = filename + ".~" + genfile::string_utils::to_string( filename_stack.size() ) ;
 		filename_stack.push_back( next_backup ) ;
 		if( filename_stack.size() > m_max_number_of_backups ) {
 			if( BFS::exists( next_backup )) {
